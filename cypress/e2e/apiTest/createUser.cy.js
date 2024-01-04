f4f114c7ce73f364c7b018efe2cc2172f3a8d14b
bonfatti <carlosbonfatti1@gmail.com>
2024-01-03 22:54:31 -0300

    third commit

--- cypress/e2e/apiTest/createUser.cy.js
+++ cypress/e2e/apiTest/createUser.cy.js
@@ -5,20 +5,20 @@ ajv.addMetaSchema(draft7MetaSchema);
 import { faker } from "@faker-js/faker";
 
 describe("POST /users", () => {
-    it("register a new user", () => {
-      const userFaker = {
-        nome: faker.person.firstName(),
-        username: faker.person.lastName(),
-        email: faker.internet.email(),
-      };
-  
-      cy.api({
-        url: "/users",
-        method: "POST",
-        body: userFaker,
-      }).then((response) => {
-        expect(response.status).to.eq(201); // Verifica se o usuário foi criado com sucesso
-        expect(response.body).to.have.property("id"); // Verifica se o usuário tem um ID
-      });
+  it("register a new user", () => {
+    const userFaker = {
+      nome: faker.person.firstName(),
+      username: faker.person.lastName(),
+      email: faker.internet.email(),
+    };
+
+    cy.api({
+      url: "/users",
+      method: "POST",
+      body: userFaker,
+    }).then((response) => {
+      expect(response.status).to.eq(201);
+      expect(response.body).to.have.property("id");
     });
   });
+});
--- cypress/e2e/apiTest/updateUsers.cy.js
+++ cypress/e2e/apiTest/updateUsers.cy.js
@@ -9,26 +9,10 @@ describe("PUT /users", () => {
   it("update a user", () => {
     cy.getUser().then((response) => {
       expect(response.status).to.eq(200);
-
-      // Verifica se a resposta possui um corpo (body) e se o corpo é uma lista de usuários
       expect(response.body).to.be.an("array");
-
-      // Obtém a lista de usuários do corpo da resposta
-      const users = response.body.users;
-
-      // Verifica se a lista de usuários não está vazia
       expect(response.body.length).to.be.greaterThan(0);
-
-      // Escolhe aleatoriamente um índice da lista de usuários
       const randomIndex = Math.floor(Math.random() * response.body.length);
-
-      // Obtém o ID do usuário no índice sorteado
-
       let id = response.body[randomIndex].id;
-
-      // Agora você pode usar o ID sorteado conforme necessário
-      cy.log(`Random User ID: ${id}`);
-
       const user = {
         nome: faker.person.firstName(),
         username: faker.person.lastName(),
@@ -36,14 +20,12 @@ describe("PUT /users", () => {
       };
 
       cy.api({
-        url: `/users/${id}`, // Use o ID gerado anteriormente
+        url: `/users/${id}`,
         method: "PUT",
         body: user,
         failOnStatusCode: false,
       }).then((response) => {
-        // Adicione aqui suas validações para a resposta da API
-        expect(response.status).to.eql(200); // Verifica se a atualização foi bem-sucedida
-
+        expect(response.status).to.eql(200);
         cy.fixture("updateSchema").then((schema) => {
           const validate = ajv.compile(schema);
           const isValid = validate(response.body);
